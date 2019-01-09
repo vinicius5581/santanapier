196cc0b1396334265b55f6672dfcd22f38189e49
Vinicius Santana <santanaviny@gmail.com>
2019-01-08 20:45:10 -0800

    adding address

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import About from './About';
 import Contact from './Contact';
+import Social from './Social';
 import './App.css';
 
 class App extends Component {
@@ -11,10 +12,15 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <h1>Santana Pier</h1>
+            <p>Amazon seller</p>
             <Route path="/about/" component={About} />
             <Route path="/contact/" component={Contact} />
             <Route path="/social/" component={Social} />
-          </header>          
+          </header>       
+          <footer className="App-footer">
+            <p>501 Delancey St. #110 - San Francisco - CA - 94107</p>
+            <p>(415) 312-5802</p>
+          </footer>   
         </div>        
       </Router>
     );
